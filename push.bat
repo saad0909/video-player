822c21747e649313b3c7407618e2b0053f54b17c
saad0909 <saadatif45@gmail.com>
2024-05-14 13:25:30 +0500

    new user

---
+++ push.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal enabledelayedexpansion
+
+git config --global user.email "saadatif45@gmail.com"
+git config --global user.name "saad0909"
+
+git add .
+git commit -m "new user"
+git push origin RazaMehdi-patch-1
+
+pause
